893a873f57181ff088a3e520a9ee727494bc7577
Roberto <robertoo.jof@gmail.com>
2022-05-21 03:13:55 -0300

    .

--- src/application/App.java
+++ src/application/App.java
@@ -1,7 +1,18 @@
 package application;
 
+import boardLayer.Board;
+import boardLayer.Position;
+import chess.ChessMatch;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Chess!");
+        //System.out.println("Hello, Chess!");
+        //Board board = new Board(8, 8);
+
+        ChessMatch chessMatch = new ChessMatch();
+
+        //chessMatch.getPieces();
+
+        UI.printBoard(chessMatch.getPieces());
     }
 }
